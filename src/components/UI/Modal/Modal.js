d1803d3467f9804e3682d293b5c5b2c6926aecf0
David Shin <xdavidshinx@gmail.com>
2020-07-12 14:13:05 -0700

    add sidebar

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Backdrop from '../Backdrop/Backdrop'
 
 import classes from './Modal.module.css'
 
-const modal = (props) => (
+class Modal extends Component {
 
-  <div>
-    <Backdrop show={props.show} clicked={props.modalClosed}/>
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1': '0'
-      }}
-      >
-      {props.children}
-    </div>
-  </div>
-)
+  shouldComponentUpdate(nextProps, nextState) {
+      return nextProps.show !== this.props.show
+  }
 
-export default modal;
+  componentWillUpdate() {
+    console.log('[MODEL] WillUpdate')
+  }
+  render () {
+    return (
+      <div>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1': '0'
+          }}
+          >
+          {this.props.children}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Modal;
